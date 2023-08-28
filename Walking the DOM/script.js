6423e4446417fe4f8b6f3c832e9c1b65291b921a
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2023-08-28 21:45:48 +0530

    children of an element

--- Walking the DOM/script.js
+++ Walking the DOM/script.js
@@ -1,2 +1,9 @@
-
 // document.getElementsByClassName("cursor-pointer")[5].style.color = "red"
+
+// children of an element
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)
+
+let arr = Array.from(document.body.childNodes)
+console.log(arr)
