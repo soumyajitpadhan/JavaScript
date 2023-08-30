100477a6601651091edaec31d2e24abacdac6812
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2023-08-30 19:48:38 +0530

    matches_closest_contains

---
+++ matches_closest_contains/script.js
@@ -0,0 +1,14 @@
+// console.log(id1);//THis is not a good practice
+
+let id1 = document.getElementById("id1")
+let sp1 = document.getElementById("sp1")
+console.log(id1)
+console.log(id1.matches(".class"))
+console.log(id1.matches(".box"))
+
+console.log(id1.closest(".box"))
+console.log(sp1.closest("#sp1"))
+
+console.log(id1.contains(sp1))
+console.log(sp1.contains(id1))
+console.log(sp1.contains(sp1))
