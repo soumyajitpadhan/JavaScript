2fb72a059cd9ffb447c0f877ded19b76bcf80942
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2023-09-02 22:40:31 +0530

    jokes website

---
+++ Exercise - 3/script.js
@@ -0,0 +1,605 @@
+let myjokes = [`Why don't the circus lions eat the clowns?
+Because they taste funny!`,
+
+    `What did the pirate say when he turned 80?
+"Aye matey."`,
+
+    `Why did the chicken cross the playground?
+To get to the other slide!`,
+
+    `Why did the actor fall through the floorboards?
+They were going through a stage!`,
+
+    `Why did a scarecrow win a Nobel prize?
+He was outstanding in his field!`,
+
+    `Why are peppers the best at archery?
+Because they habanero!`,
+
+    `What did the duck say after she bought chapstick?
+Put it on my bill!`,
+
+    `Why did an old man fall in a well?
+Because he couldn’t see that well!`,
+
+    `What do you call a fake noodle?
+An impasta!`,
+
+    `What did the three-legged dog say when he walked into a saloon?
+“I’m looking for the man who shot my paw!”`,
+
+    `How do you tell the difference between a bull and a cow?
+It is either one or the udder!`,
+
+    `What’s red and smells like blue paint?
+Red paint!`,
+
+    `What’s the difference between a hippo and a Zippo?
+One is very heavy, the other is a little lighter!`,
+
+    `What happened when Bluebeard fell overboard in the Red Sea?
+He got marooned!`,
+
+    `Why couldn't the skeleton go to school?
+His heart just wasn’t in it.`,
+
+    `What did the termite say when it walked into a bar?
+"Where's the bar tender?"`,
+
+    `Why can’t you send a duck to space?
+Because the bill would be astronomical!`,
+
+    `What does Jeff Bezos do before he goes to sleep?
+He puts his PJ-Amazon!`,
+
+    `What happened when the world's tongue-twister champion got arrested?
+They gave him a tough sentence!`,
+
+    `What did the mama cow say to the calf?
+It’s pasture bedtime!`,
+
+    `How does a vampire start a letter?
+Tomb it may concern!`,
+
+    `What do you call an illegally parked frog?
+Toad!`,
+
+    `What did one plate say to the other?
+Dinner is on me!`,
+
+    `Why do hummingbirds hum?
+Because they don’t know the words !`,
+
+    `What do sprinters eat before a race?
+Nothing. They fast!`,
+
+    `Two muffins are baking in an oven. One of them looks to the other and says, "Phew, it's getting hot in here!" The other looks back and says, "Ack! A talking muffin!"`,
+
+    `What kind of cat likes living in water?
+An octo-puss.`,
+
+    `Why did the dinosaur cross the road?
+Because chickens hadn't evolved yet.`,
+
+    `What do you call a cow that can't make milk?
+An udder failure — a milk dud.`,
+
+    `What do cats eat for breakfast?
+Mice Crispies!`,
+
+    `What do you call an elephant that doesn’t matter?
+An irrelephant!`,
+
+    `What do you get when you cross a rabbit with shellfish?
+An oyster bunny!`,
+
+    `Where do polar bears keep their money?
+In a snow bank!`,
+
+    `Why did the pony get sent to his room?
+He wouldn’t stop horsing around!`,
+
+    `What kind of dog does a magician have?
+A Labracadabrador!`,
+
+    `Where do cows go on Friday nights?
+They go to the moo-vies!`,
+
+    `Why couldn't the pony sing “Happy Birthday?”
+Because she was just a little hoarse!`,
+
+    `How do you make an octopus laugh?
+With ten-tickles!`,
+
+    `How do you keep a bull from charging?
+Take away its credit card!`,
+
+    `Why can’t a leopard hide?
+Because he’s always spotted !`,
+
+    `How do you get a squirrel to like you?
+Act like a nut!`,
+
+    `What is the difference between a cat that took a nap on the Xerox machine and a cat that imitates everything you do?
+One is a cat copy, and the other is a copy cat.`,
+
+    `How do young bees get to school?
+They take the school buzz!`,
+
+    `What do frogs order at fast-food restaurants?
+French flies!`,
+
+    `What do you get from a pampered cow?
+Spoiled milk!`,
+
+    `What do you call an alligator who solves mysteries?
+An investigator!`,
+
+    `Why is a snake difficult to fool?
+You can’t pull its leg!`,
+
+    `What kind of socks do grizzlies wear?
+None, they have bear feet!`,
+
+    `What do you get when you cross a snail with a porcupine?
+A slowpoke!`,
+
+    `What did the dog say when it sat on sandpaper?
+"Ruff!"`,
+
+    `What's a cat's favorite dessert?
+Chocolate mouse!`,
+
+    `What fish only swims at night?
+Starfish!`,
+
+    `What does a triceratops sit on?
+Its tricera-bottom!`,
+
+    `How does a cucumber become a pickle?
+It goes through a jarring experience.`,
+
+    `What's a turnip's favorite soda?
+Root beer!`,
+
+    `What happened when the cheese shop exploded?
+Da brie was everywhere!`,
+
+    `What’s a piece of bread’s least favorite chore?
+Doing a loaf of laundry.`,
+
+    `What did the bunny say to the carrot?
+It’s been nice gnawing you!`,
+
+    `What do you call a sad strawberry?
+A blueberry!`,
+
+    `Why did the tomato blush?
+Because it saw the salad dressing!`,
+
+    `What did the pizza say to the topping?
+I never sau-sage a pretty face!`,
+
+    `Which vegetable do sailors hate the most?
+Leeks!`,
+
+    `What do you call a cheese that’s not yours?
+Nacho cheese!`,
+
+    `Why did the cookie go to the nurse?
+Because he felt crummy!`,
+
+    `What kind of room doesn’t have doors?
+A mushroom!`,
+
+    `What kind of key opens a banana?
+A mon-key!`,
+
+    `What happens when a grape gets run over crossing the street?
+A traffic jam!`,
+
+    `What kind of fruit do scarecrows love the most?
+Straw-berries!`,
+
+    `Why couldn't the sesame seed leave the casino?
+Because it was on a roll`,
+
+    `How do you make an apple turnover?
+Push it down a hill!`,
+
+    `What does garlic do when it gets hot?
+It takes its cloves off!`,
+
+    `Where does fruit go on vacation?
+Pear-is!`,
+
+    `Why did the melons choose not to get married?
+Because they cantaloupe!`,
+
+    `What does a lemon say when it answers the phone?
+Yellow!`,
+
+    `What did one dried fruit say when another asked it to the movies?
+It's a date!`,
+
+    `Why do bees have sticky hair?
+They use honeycombs!`,
+
+    `What does a cow call an earthquake?
+A milkshake!`,
+
+    `Why did the man go to the yogurt museum?
+To get a little culture!`,
+
+    `Why couldn't the sesame seed climb up the hill?
+Because it was on a roll!`,
+
+    `Why can't you trust tacos?
+Because they always spill the beans!`,
+
+    `Why is the obtuse triangle always frustrated?
+Because it’s never right!`,
+
+    `Why do plants hate math?
+Because it's full of square roots!`,
+
+    `What do you call people who really like tractors?
+Protractors!`,
+
+    `How come no one picks statistics as their favorite subject?
+It’s just average.`,
+
+    `Why did two 4s skip dinner?
+Because they already 8!`,
+
+    `Did you hear about the mathematician who’s afraid of negative numbers?
+He’ll stop at nothing to avoid them!`,
+
+    `What did the triangle feel sorry for the circle?
+Because it’s pointless!`,
+
+    `Why was the math book sad?
+It had too many problems!`,
+
+    `Did you hear about the rancher who had 97 cows in his field?When he rounded them up, he had 100!
+`,
+    `Why couldn’t the angle get a loan?
+Because his parents wouldn’t cosine!`,
+
+    `What do you call a number that can’t stay in one place?
+A roamin’ numeral!`,
+
+    `Why should you never trust someone writing on graph paper?
+Because they must be plotting something!`,
+
+    `What do you call two guys who love math?
+Algebros!`,
+
+    `What do you get when you divide a Jack o' lantern by its diameter?
+Pumpkin pi!`,
+
+    `Why was the fraction nervous about marrying the decimal?
+Because it would have to convert!`,
+
+    `Why was the triangle the MVP of the basketball team?
+It always made three-pointers!`,
+
+    `What do you call a hen who counts her own eggs?
+A mathema-chicken!`,
+
+    `What did the 90° angle say after an argument?
+"It turns out, I was right!"`,
+
+    `What tool do mathematicians use most?
+Multi-pliers!`,
+
+    `Why did the student get upset when their teacher called them average?
+It was a mean thing to say!`,
+
+    `If a math teacher had four apples in one hand and five apples in the other hand, what would they have altogether?
+Really big hands!`,
+
+    `What did the 0 say to the 8?
+Nice belt!`,
+
+    `Why was 6 afraid of 7?
+Because 7, 8, 9!`,
+
+    `Why should you never start a conversation with pi?
+It'll just go on forever!`,
+
+    `What is a bird’s favorite type of math?
+OWL-gebra!`,
+
+    `Why do teenagers travel in groups of threes and fives?
+Because they can’t even!`,
+
+    `Did you hear about the the circle that kept going to school?
+It has 360°!`,
+
+    `Where would you be most likely to find an elephant?
+The same place you lost one!`,
+
+    `Why did the football coach yell at the vending machine?
+They wanted their quarter back!`,
+
+    `What do you call a bear without any teeth?
+A gummy bear!`,
+
+    `Why did the robber take a bath before he left the scene of the crime?
+He wanted to make a clean getaway!`,
+
+    `Why does it take pirates a long time to learn the alphabet?
+Because they can spend years at C!`,
+
+    `What did one snowman say to the other snowman?
+It smells like carrots over here!`,
+
+    `What did one wall say to the other wall?
+I’ll meet you at the corner!`,
+
+    `Why did the kid throw his clock out the window?
+Because he wanted to see time fly!`,
+
+    `What do you call a huge pile of cats?
+A meow-ntain!`,
+
+    `Why wouldn’t the shrimp share his treasure?
+Because he was a little shellfish!`,
+
+    `Why is Cinderella bad at soccer?
+Because she’s always running away from the ball!`,
+
+    `What does a rain cloud wear under its clothes?
+Thunderwear!`,
+
+    `Why do vampires seem sick all the time?
+Because they’re always coffin!`,
+
+    `How do you get an astronaut’s baby to stop crying?
+You rocket!`,
+
+    `What is fast, loud and crunchy?
+A rocket chip!`,
+
+    `What’s the best thing about Switzerland?
+I don’t know, but the flag is a big plus!`,
+
+    `Did you hear about the claustrophobic astronaut?
+They just needed a little space.`,
+
+    `Why are ghosts bad liars?
+Because you can see right through them !`,
+
+    `Why are spiders so smart?
+They can find everything on the web!`,
+
+    `What are mummies' favorite lunches?
+Wraps!`,
+
+    `How does Darth Vader like his toast?
+On the dark side!`,
+
+    `Why are mountains so funny?
+They're just hill areas!`,
+
+    `Why did the student eat his homework?
+Because his teacher told him it would be a piece of cake! `,
+
+    `What is the Easter bunny’s favorite type of music?
+Hip-hop!`,
+
+    `Why did Beethoven get rid of his chickens?
+All they ever said was, “Bach, Bach, Bach!”`,
+
+    `Why did the man name his dogs Rolex and Timex?
+Because they were watchdogs!`,
+
+    `Why is grass so dangerous?
+Because it’s full of blades!`,
+
+    `What did the Dalmatian say after lunch?
+That hit the spot!`,
+
+    `What do you call a bee that can't make up its mind?
+A maybe!`,
+
+    `What did one volcano say to the other?
+I lava you!`,
+
+    `What did the teacher say when a book fell on their head?
+I have only my shelf to blame.`,
+
+    `What kind of tree can fit in one hand?
+A palm tree!`,
+
+    `How do they keep the basketball arena cool?
+They fill it with fans!`,
+
+    `What does a book do to keep warm in the winter?
+It puts on a jacket!`,
+
+    `What did the lightbulb say to its sweetheart?
+I wuv you a watt!`,
+
+    `What’s the difference between a guitar and a fish?You can tune a guitar, but you can’t tuna fish!
+`,
+    `What did the fisherman say to the magician?
+Pick a cod, any cod!`,
+
+    `Why did the picture go to prison?
+Because it was framed!`,
+
+    `Why do bicycles fall over?
+Because they’re two-tired!`,
+
+    `If a seagull flies over the sea, what flies over the bay?
+A bagel!`,
+
+    `What does the ocean do when it sees its friends?
+It waves!`,
+
+    `What do you get when you mix a cocker spaniel, a poodle, and a ghost?
+A cocker-poodle boo!`,
+
+    `What is a tornado’s favorite game to play?
+Twister`,
+
+    `How did the students feel when they learned about electricity?
+Totally shocked!`,
+
+    `Why did the golfer wear two pairs of pants?
+Just in case he got a hole in one!`,
+
+    `Why was the broom late?
+It over-swept!`,
+
+    `What did the paper say to the pencil?
+Write on!`,
+
+    `What do you call a belt made of watches?
+A waist of time!`,
+
+    `Where do sailboats go when they're sick?
+To the dock!`,
+
+    `How does the moon cut his hair?
+Eclipse it!`,
+
+    `What do you do when a lemon gets sick?
+You give it lemon-aid!`,
+
+    `What’s worse than raining cats and dogs?
+Hailing taxis!`,
+
+    `How can you tell if a tree is a dogwood tree?
+By its bark!`,
+
+    `What do astronauts do before throwing a party?
+They planet!`,
+
+    `What kind of water can’t freeze?
+Hot water!`,
+
+    `Why do we put candles on top of birthday cakes?
+Because it's hard to light them from the bottom!`,
+
+    `Why didn't the shopper buy the camo pants they wanted?
+They couldn't find any!`,
+
+    `What did the family say when they lost 25% of their roof?
+Oof.`,
+
+    `Why are elevator jokes so good?
+They work on many levels!`,
+
+    `What's worse than finding a worm in your apple?
+Finding half a worm in your apple!`,
+
+    `What do you call a boomerang that doesn't come back?
+A stick!`,
+
+    `What do you call a bear with no ears?
+A “B!”`,
+
+    `What do pirates pay for corn?
+A buck an ear!`,
+
+    `Which hand is it better to write with?
+Neither, it’s better to write with a pencil!`,
+
+    `What do you call a fish with no eye?
+A fsh!`,
+
+    `Why are balloons so expensive?
+Inflation!`,
+
+    `What breed of dog can jump higher than a skyscraper?
+Any breed of dog. Skyscrapers can’t jump!`,
+
+    `Why can't you trust atoms?
+They make up everything!`,
+
+    `Did you hear about the woman whose left side was cut off?
+She's all right now.`,
+
+    `Why did it take so long for a man to eat a clock?
+It was very time-consuming.`,
+
+    `What’s the one thing will you get every year on your birthday, guaranteed?
+A year older!`,
+
+    `Did you hear about the unemployed personal trainer?
+They gave their too-weak notice!`,
+
+    `What's brown and sticky?
+A stick!`,
+
+    `Why was the calendar afraid?
+Its days were numbered!`,
+
+    `What's E.T. short for?
+Because he's only got little legs!`,
+
+    `Did you hear about the guy who invented the knock-knock joke?
+He won the "no-bell" prize!`,
+
+    `Why can’t a hand be 12 inches long?
+Because then it would be a foot!`,
+
+    `Why is it hard to understand volunteers?
+Because they make no cents!`,
+
+    `How much does it cost Santa to park his sleigh?
+Nothing, it's on the house!`,
+
+    `How is it that I only know 25 letters of the alphabet?
+I just don't know y!`,
+
+    `When does a regular joke become a dad joke?
+When it becomes apparent!`,
+
+    `What has two legs but can't walk?
+A pair of pants!`,
+
+    `Where do you learn how to make ice cream?
+Sundae school!`,
+
+    `What do you call a lazy kangaroo?
+A pouch potato!`,
+
+    `Where do pencils come from?
+Pennsylvania!`,
+
+    `What did the cat say when it fell down the stairs?
+Me-ow!`,
+
+    `Why can't you tell a joke to an egg?
+It might crack up!`,
+
+    `Why are fish so smart?
+Because they live in schools!`,
+
+    `What did the big flower say to the little flower?
+Hi, bud!`,
+
+    `How can you make a tissue dance?
+Put a little boogie in it!`,
+
+    `What did the buffalo say when his little boy left for school?
+Bison!`,
+
+    `What animal can you always find at a baseball game?
+A bat!`,
+
+    `Why did the boy throw a stick of butter out the window?
+Because he wanted to see a butterfly!`,
+
+    `What did Baby Corn say to Mama Corn?
+"Where is Pop Corn?"`,
+
+    `What do you call a train with a cold?
+A-choo choo train!`,]
+
+let index = Math.floor(Math.random() * myjokes.length);
+joke.innerHTML = myjokes[index];
