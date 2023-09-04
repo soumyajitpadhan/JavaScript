98907cb5a68ed51b510e66d4dfee6e3715a8b2d7
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2023-09-04 19:45:10 +0530

    innerHTML, outerHTML

---
+++ innerHTML, outerHTML/script.js
@@ -0,0 +1,24 @@
+// DOM
+
+/*let x = document.getElementsByTagName("span")[0];
+console.log(x);
+let y = document.getElementsByTagName("span")[0];
+console.dir(y);
+
+console.log(document.body.firstChild.nodeName);
+console.log(document.body.firstElementChild.nodeName);*/
+
+//innerHTML
+// console.log(first.innerHTML);
+// first.innerHTML = "<i>Hey i am italic tag</i>";
+
+// outerHTML
+// console.log(first.outerHTML);
+// first.outerHTML = "<div> HI </div>";
+
+
+// document.body.firstChild
+// document.body.firstChild.data
+// document.body.firstChild.nodeValue
+// console.log(document.body.textContent);
+// first.hidden = false
