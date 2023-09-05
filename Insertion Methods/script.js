c5022b1ad389cad0b97a49a4c8d83f828fbcc368
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2023-09-05 19:28:58 +0530

    Insertion Methods

---
+++ Insertion Methods/script.js
@@ -0,0 +1,14 @@
+let a = document.getElementsByTagName('div')[0]
+
+// a.innerHTML = a.innerHTML + '<h1>Hello World</h1>';
+
+let div = document.createElement('div');
+div.innerHTML = '<h1>Hello World</h1>';
+// a.appendChild(div);
+// document.body.append(div);
+// a.append(div);
+// a.prepend(div);
+// a.before(div);
+a.after(div);
+// a.replaceWith(div)
+
