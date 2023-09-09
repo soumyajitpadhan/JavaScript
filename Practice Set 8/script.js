d2f5ca4d87d51671091637c969904b13cc08b95f
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2023-09-09 09:29:14 +0530

    Practice Set 8

---
+++ Practice Set 8/script.js
@@ -0,0 +1,28 @@
+// Question - 3
+// document.getElementById("google").addEventListener("click", function () {
+//     // let url = "https://google.com";
+//     // window.location = url
+//     window.location = "https://google.com";
+//     // win.focus();
+// })
+
+// document.getElementById("facebook").addEventListener("click", function () {
+//     let url = "https://facebook.com";
+//     window.location = url
+// })
+
+// document.getElementById("instagram").addEventListener("click", function () {
+//     window.location = "https://instagram.com";
+//     // console.log(window.location)
+// })
+
+// Question - 4
+// setInterval(async function() {
+//     let url = "https://jsonplaceholder.typicode.com/todos/1";
+//     console.log(await fetchContent(url));
+// },3000)
+
+// Question - 5
+// setInterval(function(){
+//     document.querySelector("#bulb").classList.toggle("bulb");
+// },300)
