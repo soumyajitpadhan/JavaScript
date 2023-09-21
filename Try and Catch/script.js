5f01a821613a016dedaa75d100a9e67637f47d93
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2023-09-21 19:41:43 +0530

    Try and Catch

---
+++ Try and Catch/script.js
@@ -0,0 +1,50 @@
+setTimeout(() => {
+    console.log("Hacking wifi.....Please wait.....");
+}, 1000)
+
+try {
+    console.log(rahul);
+}
+catch (error) {
+    console.log("Error is : " + error);
+}
+
+/*try {
+    setTimeout(() => {
+        console.log(rahul);
+    }, 100)
+}
+catch (error) {
+    console.log("Error is : " + error);
+}*/
+
+/*try {
+    setTimeout(() => {
+        try {
+            console.log(rahul);
+        }
+        catch (err) {
+
+        }
+
+    }, 100)
+}
+catch (error) {
+    console.log("Error is : " + error);
+}*/
+
+setTimeout(() => {
+    console.log("Fetching username and password.....Please wait.....");
+}, 2000)
+
+setTimeout(() => {
+    console.log("Hacking Rahul's facebook id.....Please wait.....");
+}, 3000)
+
+setTimeout(() => {
+    console.log("Username and password of Rahul (+917689566547) fetched.....Please wait.....");
+}, 4000)
+
+
+
+
