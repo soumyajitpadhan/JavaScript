61284983cbc02050145ed0ebcd67893febac9087
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2023-09-23 09:14:34 +0530

    Finally

---
+++ Finally Clause/script.js
@@ -0,0 +1,33 @@
+try {
+    let a = 0;
+    console.log(program);
+    console.log("Program ran successfully.");
+
+} catch (err) {
+    console.log("This is an error.");
+    console.log(p);
+} finally {
+    console.log("I am a good boy.");
+    //Close the file
+    //Exit the Loop
+    // Write to the log file
+}
+
+
+/*const f = () => {
+    try {
+        let a = 0;
+        // console.log(program);
+        console.log("Program ran successfully.");
+        return;
+
+    } catch (err) {
+        console.log("This is an error.");
+        console.log(p);
+    } finally {
+        console.log("I am a good boy.");
+    }
+}
+
+f();
+console.log("End");*/
