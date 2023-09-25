c5d58357beaaf7088657f288664be34e5a601eed
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2023-09-25 22:16:45 +0530

    HakerMan

---
+++ Exercise - 5/script.js
@@ -0,0 +1,37 @@
+let a = [
+    "Initializing Hack tool.......",
+    "Connecting to Facebook.......",
+    "Connecting to server 1.......",
+    "Connection failed. Retrying.......",
+    "Connecting to server 2.......",
+    "Connected Successfully.......",
+    "Username ashish.......",
+    "Trying Brute Force.......",
+    "200k passwords tried.......",
+    "Match not found.......",
+    "Another 200k passwords tried.......",
+    "Match found.......",
+    "Accessing Account.......",
+    "Hack Successful......."
+]
+
+const sleep = async (seconds) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(true)
+        }, seconds * 1000)
+    })
+}
+
+const showHack = async (message) => {
+    await sleep(2);
+    // console.log(message);
+    text.innerHTML = text.innerHTML + message + "<br>"
+
+}
+
+(async () => {
+    for (let i = 0; i < a.length; i++) {
+        await showHack(a[i]);
+    }
+})()
